6a2fe94f3aa1a0b62536737ece58ed730cd74893
RDEX-J5 <rdexgaming@gmail.com>
2026-05-12 15:50:25 -0600

    Practica Finalizada

--- Practica1/holamundo.js
+++ Practica1/holamundo.js
@@ -12,15 +12,15 @@ console.log((edad1+edad2)/2)
 /* medir el timepo del proceso*/
 console.time("miProceso")
 
-for(let i =0; i< 1000000000000; i++){  }
+for(let i =0; i< 1000000; i++){  }
 
 console.timeEnd("miProceso")
 
 /* Objetivo tipo tabla */
 
 let usuarios=[
-    {nombre: "ivan", edad: 38},
-    {nombre: "isay", edad: 38},
+    {nombre: "Cristian", edad: 20},
+    {nombre: "Erasto", edad: 20},
 ];
 
-console.table(ususarios);
+console.table(usuarios)
