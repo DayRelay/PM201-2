ff17403018d5d3ecc660dd26854333fc0185405a
RDEX-J5 <rdexgaming@gmail.com>
2026-06-09 11:12:18 -0600

    Agrega practica 6

---
+++ Practica 6/appTest/App.js
@@ -0,0 +1,36 @@
+/*Zona 1: Importacion de archivos y componentes*/ 
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
+
+/*Zona 2: Main -Componentes*/
+
+export default function App() {
+  return (
+
+    <View style={styles.container}>
+
+      <Image source= {require('./assets/wave.png')}/>
+
+      <Text>Hola Mundo Reac Native </Text>
+      <Text>------------------------------</Text>
+      <Saludo/>
+      <Text>------------------------------</Text>
+      <Saludo2/>
+      <StatusBar style="auto" />
+
+
+    </View>
+  );
+}
+/*Zona 3: Estilos y posicionamiento*/
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Practica 6/appTest/components/Perfil.js
@@ -0,0 +1,11 @@
+import {view, Text} from 'react-native';
+
+export const Perfil = () => {
+    return(
+        <View>
+            <Text>Nombre: Cristian Erasto Corona Uribe</Text>
+            <Text>Ingeniería en Sistemas Computacionales</Text>
+            <Text>Asignatura: Programación Móvil</Text>
+        </View>
+    )
+}
